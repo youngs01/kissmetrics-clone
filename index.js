4d6821ed1fe196366c1b647cc81ed1180631dce0
youngs01 <youngscorporations0319@gmail.com>
2024-02-16 03:04:14 -0800

    Testing 1.0
    This website is half build and to test the responsiveness in different devices this is the second push on 16/2/2024

--- index.js
+++ index.js
@@ -36,4 +36,32 @@ window.addEventListener("resize", function () {
 marqueeSlider();
 })
 
-var myAccordion = new bootstrap.Accordion(document.getElementById('accordionExample'));
+
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+});
+
+var swiper = new Swiper(".mySwiper3", {
+  slidesPerView: 3,
+  spaceBetween: 2,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+
+
