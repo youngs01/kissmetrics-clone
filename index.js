b7ec3a667cd42a788ba5cbfc4cd282a805042c08
youngs01 <youngscorporations0319@gmail.com>
2024-02-29 04:31:23 -0800

    fadeanimation added

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-let marqueeSliderView ;
-function marqueeSlider (){
-    
-    if(window.innerWidth <= 500){
-     marqueeSliderView = 2.7;
-    }
+let marqueeSliderView;
+function marqueeSlider() {
 
-    else if(window.innerWidth <= 769){
-        marqueeSliderView = 3
-          }
-    else if(window.innerWidth <= 1600){
-     marqueeSliderView = 4;
-    }
+  if (window.innerWidth <= 500) {
+    marqueeSliderView = 2.7;
+  }
+
+  else if (window.innerWidth <= 769) {
+    marqueeSliderView = 3
+  }
+  else if (window.innerWidth <= 1600) {
+    marqueeSliderView = 4;
+  }
 
 
-    else{
-marqueeSliderView = 5;
-    }
+  else {
+    marqueeSliderView = 5;
+  }
 
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: marqueeSliderView,
-        loop: true,
-        speed: 5000,
-        effect: "slide",
-        grabCursor: true,
-        autoplay: {
-          delay: 0,
-          disableOnInteraction: false,
-        },
-      });
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: marqueeSliderView,
+    loop: true,
+    speed: 5000,
+    effect: "slide",
+    grabCursor: true,
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    },
+  });
 }
 
 marqueeSlider();
 
 window.addEventListener("resize", function () {
-marqueeSlider();
+  marqueeSlider();
 })
 
 
@@ -46,8 +46,8 @@ var swiper = new Swiper(".mySwiper2", {
     clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-btn-next",
+    prevEl: ".swiper-btn-prev",
   },
 
 });
@@ -64,4 +64,31 @@ var swiper = new Swiper(".mySwiper3", {
 
 
 
+document.addEventListener('DOMContentLoaded', function () {
+  var controller = new ScrollMagic.Controller();
 
+  var ourScene = new ScrollMagic.Scene({
+    triggerElement: '#fadeSignContainer'
+  })
+    .setClassToggle('#fadeSignContainer', 'fade-in')
+    .addTo(controller);
+
+
+});
+
+
+const range = document.getElementById('myRange');
+const eventsOutput = document.getElementById('rangeNumber');
+
+if (range.value == 100) {
+  eventsOutput.innerHTML = "100M";
+} else {
+  eventsOutput.innerHTML = range.value + "K";
+}
+range.addEventListener('input', function () {
+  if (this.value == 100) {
+    eventsOutput.innerHTML = "100M";
+  } else {
+    eventsOutput.innerHTML = this.value + "K";
+  }
+});
